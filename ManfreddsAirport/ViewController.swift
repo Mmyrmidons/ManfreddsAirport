1afb2d94d490609bf60b7b3850b417d6f33b32c8
BrotherGrilka <exweerdo@gmail.com>
2017-01-30 10:02:05 -0500

    Initial Commit

---
+++ ManfreddsAirport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManfreddsAirport
+//
+//  Created by Dunc on 1/30/17.
+//  Copyright © 2017 Mmyrmidons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
